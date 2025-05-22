0f5ea5869d38c227a4a3e3c26dc4fb157c9e23f0
Martin L. <77213543+Vive-bit@users.noreply.github.com>
2025-05-22 10:22:50 +0200

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,9 +1,1 @@
-document.addEventListener("message", function(event) {
-    const data = event.data;
-
-    const result = split_(data);
-
-    window.ReactNativeWebView.postMessage(JSON.stringify(result));
-  });
-
-function split_(txt) {return txt.split("|");} // to split ai response for data tables
+function doPost(e) {return e.parameters.prompt.split("|");} // to split ai response for data tables
