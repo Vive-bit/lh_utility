f7978487e1b6b376d104851a08fc6ca6a0d65afb
Martin L. <77213543+Vive-bit@users.noreply.github.com>
2025-05-28 15:42:12 +0200

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,1 +1,7 @@
-function doPost(e) {return "s";} // to split ai response for data tables
+function doPost(e) {
+  var type_ = e.parameter.type
+  var result = {"code": "ok"};
+  if (type_ == "split") {result["prompt"] = e.parameter.prompt.split("|")}}
+  
+  return ContentService.createTextOutput(JSON.stringify(result)).setMimeType(ContentService.MimeType.JSON);
+} 
